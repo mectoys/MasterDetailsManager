e307b16f1f7457d57541b59575cd377c2b98c659
highchart_samples <mectoys2013@gmail.com>
2025-03-18 20:56:13 -0500

    Implementación de capa DAL y capa BLL agregando todos los metodos

---
+++ MasterDetailsManager/DAL/FacturaBLL.cs
@@ -0,0 +1,53 @@
+﻿using DAL;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace DAL
+{
+    public class FacturaBLL
+    {
+        private readonly FacturaRepository _facturaRepository = new FacturaRepository();
+
+        public DataSet ObtenerFactura()
+        {
+            string query = "select Id,Cliente,Fecha from factura";
+            return _facturaRepository.ExecuteQuery(query);
+        }
+
+        public int CrearFactura(Factura factura)
+        {
+            if (factura == null || factura.Detalles == null || factura.Detalles.Count == 0)
+                throw new ArgumentException("La factura o sus detalles no son válidos.");
+
+            // Llamar al método transaccional desde el repositorio
+            _facturaRepository.InsertarFacturaConDetalles(factura, factura.Detalles);
+
+            // Retornar el ID de la factura creada
+            return factura.Id; // Asume que `Id` se asigna dentro del método InsertarFacturaConDetalles
+        }
+
+        public void ActualizarFactura(Factura factura)
+        {
+            if (factura == null || factura.Detalles == null)
+                throw new ArgumentException("La factura o sus detalles no son válidos.");
+
+            // Obtener el detalle actual desde la base de datos
+            var detallesExistentes = _facturaRepository.ObtenerDetalleporFactura(factura.Id);
+
+            // Identificar detalles a agregar
+            var nuevosDetalles = factura.Detalles.FindAll(d => d.Id == 0);
+
+            // Identificar detalles a actualizar
+            var detallesActualizados = factura.Detalles.FindAll(d => d.Id != 0);
+
+            // Identificar detalles a eliminar
+            var detallesAEliminar = detallesExistentes.FindAll(e => !factura.Detalles.Exists(d => d.Id == e.Id));
+
+            // Transacción para manejar todos los cambios
+            _facturaRepository.ActualizarFacturaConDetalles(factura, nuevosDetalles, detallesActualizados, detallesAEliminar);
+        }
+
+    }
+}
---
+++ MasterDetailsManager/DAL/FacturaRepository.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Repositories;
+using Models;
+using MySql.Data.MySqlClient;
+
+namespace DAL
+{
+    public class FacturaRepository
+    {
+        public readonly DBConnection dBConnection = new DBConnection();
+
+        public DataSet ExecuteQuery(string sql)
+        {
+            using (var conn = dBConnection.GetConnection())
+            {
+                conn.Open();
+                MySqlDataAdapter dataAdapter = new MySqlDataAdapter(sql, conn);
+                DataSet dataSet = new DataSet();
+                dataAdapter.Fill(dataSet, "Factura");
+                return dataSet;
+            }
+        }
+
+
+
+        public void InsertarFacturaConDetalles(Factura factura, List<FacturaDetalle> detalles)
+        {
+            if (factura == null || detalles==null || detalles.Count==0)
+                throw new ArgumentException("La factura o los detalles no son válidos.");
+
+            using (var conn= dBConnection.GetConnection())
+            {
+                conn.Open();
+                using (var transaction= conn.BeginTransaction())
+                {
+                    try
+                    {
+                        var insertFacturaSql = @"INSERT INTO voucherdb.factura (Cliente, Fecha) 
+                                         VALUES (@Cliente, @Fecha);
+                                         SELECT LAST_INSERT_ID();";
+
+                        int facturaId;
+                        using (var commandFactura= new MySqlCommand(insertFacturaSql,conn,transaction))
+                        {
+                            commandFactura.Parameters.AddWithValue("@Cliente", factura.Cliente);
+                            commandFactura.Parameters.AddWithValue("@Fecha", factura.Fecha);
+                            facturaId = Convert.ToInt32(commandFactura.ExecuteScalar());
+                        }
+                        var insertDetalleSql = @"INSERT INTO facturadetalle (FacturaId, Producto, Cantidad, Precio) 
+                                         VALUES (@FacturaId, @Producto, @Cantidad, @Precio)";
+                        foreach (var detalle in detalles)
+                        {
+                            using (var commandDetalle = new MySqlCommand(insertDetalleSql, conn, transaction))
+                            {
+                                commandDetalle.Parameters.AddWithValue("@FacturaId", facturaId);
+                                commandDetalle.Parameters.AddWithValue("@Producto", detalle.Producto);
+                                commandDetalle.Parameters.AddWithValue("@Cantidad", detalle.Cantidad);
+                                commandDetalle.Parameters.AddWithValue("@Precio", detalle.Precio);
+                                commandDetalle.ExecuteNonQuery();
+                            }
+
+                        }
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+
+                        transaction.Rollback();
+                        throw new Exception("Error al insertar la factura y detalles. Transacción revertida.", ex);
+
+                    }
+                }
+            }
+
+        }
+
+        public void ActualizarFacturaConDetalles(Factura factura, List<FacturaDetalle> nuevos, List<FacturaDetalle> actualizados, List<FacturaDetalle> eliminados)
+        {
+            using (var conn = dBConnection.GetConnection())
+            {
+                conn.Open();
+                using (var transaction = conn.BeginTransaction())
+                {
+                    try
+                    {
+                        // Actualizar la factura principal
+                        var queryFactura = @"UPDATE factura SET Cliente = @Cliente, Fecha = @Fecha WHERE Id = @Id";
+                        using (var command = new MySqlCommand(queryFactura, conn, transaction))
+                        {
+                            command.Parameters.AddWithValue("@Cliente", factura.Cliente);
+                            command.Parameters.AddWithValue("@Fecha", factura.Fecha);
+                            command.Parameters.AddWithValue("@Id", factura.Id);
+                            command.ExecuteNonQuery();
+                        }
+
+                        // Eliminar detalles
+                        foreach (var detalle in eliminados)
+                        {
+                            var queryEliminar = "DELETE FROM voucherdb.facturadetalle WHERE Id = @Id";
+                            using (var command = new MySqlCommand(queryEliminar, conn, transaction))
+                            {
+                                command.Parameters.AddWithValue("@Id", detalle.Id);
+                                command.ExecuteNonQuery();
+                            }
+                        }
+
+                        // Insertar nuevos detalles
+                        foreach (var detalle in nuevos)
+                        {
+                            var queryInsertar = @"INSERT INTO facturadetalle (FacturaId, Producto, Cantidad, Precio)
+                                          VALUES (@FacturaId, @Producto, @Cantidad, @Precio)";
+                            using (var command = new MySqlCommand(queryInsertar, conn, transaction))
+                            {
+                                command.Parameters.AddWithValue("@FacturaId", factura.Id);
+                                command.Parameters.AddWithValue("@Producto", detalle.Producto);
+                                command.Parameters.AddWithValue("@Cantidad", detalle.Cantidad);
+                                command.Parameters.AddWithValue("@Precio", detalle.Precio);
+                                command.ExecuteNonQuery();
+                            }
+                        }
+
+                        // Actualizar detalles existentes
+                        foreach (var detalle in actualizados)
+                        {
+                            var queryActualizar = @"UPDATE facturadetalle 
+                                            SET Producto = @Producto, Cantidad = @Cantidad, Precio = @Precio 
+                                            WHERE Id = @Id";
+                            using (var command = new MySqlCommand(queryActualizar, conn, transaction))
+                            {
+                                command.Parameters.AddWithValue("@Producto", detalle.Producto);
+                                command.Parameters.AddWithValue("@Cantidad", detalle.Cantidad);
+                                command.Parameters.AddWithValue("@Precio", detalle.Precio);
+                                command.Parameters.AddWithValue("@Id", detalle.Id);
+                                command.ExecuteNonQuery();
+                            }
+                        }
+
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw new Exception("Error al actualizar la factura y detalles. Transacción revertida.", ex);
+                    }
+                }
+            }
+        }
+
+
+        public List<FacturaDetalle> ObtenerDetalleporFactura(int facturaId)
+        {
+            var detalles = new List<FacturaDetalle>();
+            using (var con = dBConnection.GetConnection())
+            {
+                con.Open();
+                var query = "select Id, FacturaId, Producto, Cantidad,Precio from facturadetalle" +
+                    "  WHERE FacturaId= @FacturaId ";
+                using (var command = new MySqlCommand(query, con))
+                {
+                    command.Parameters.AddWithValue("@FacturaId", facturaId);
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            detalles.Add(new FacturaDetalle
+                            {
+                                Id = reader.GetInt32("Id"),
+                                FacturaId = reader.GetInt32("FacturaId"),
+                                Producto = reader.GetString("Producto"),
+                                Cantidad = reader.GetInt32("Cantidad"),
+                                Precio = reader.GetDecimal("Precio")
+
+                            });
+                        }
+                    }
+                }
+                return detalles;
+            }
+
+        }
+
+
+    }
+}
